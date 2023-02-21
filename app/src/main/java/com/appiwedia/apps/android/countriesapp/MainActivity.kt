32f29a516a2eff7ed87095abda641484a6faf41c
DevClark <e.cleawrence@gmail.com>
2023-02-21 22:44:24 +0100

    add countrydialog and countryItem composable

--- app/src/main/java/com/appiwedia/apps/android/countriesapp/MainActivity.kt
+++ app/src/main/java/com/appiwedia/apps/android/countriesapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.appiwedia.apps.android.countriesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.appiwedia.apps.android.countriesapp.ui.CountriesScreen
+import com.appiwedia.apps.android.countriesapp.ui.CountriesViewModel
 import com.appiwedia.apps.android.countriesapp.ui.theme.CountriesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +17,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CountriesAppTheme {
+                val viewModel = hiltViewModel<CountriesViewModel>()
+                val state by viewModel.state.collectAsState()
+                CountriesScreen(
+                    state = state,
+                    onSelectCountry = viewModel::selectCountry,
+                    onDismissCountryDialog = viewModel::dismissCountryDialog
+                )
             }
         }
     }
---
+++ app/src/main/java/com/appiwedia/apps/android/countriesapp/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.appiwedia.apps.android.countriesapp.di
+
+import com.apollographql.apollo3.ApolloClient
+import com.appiwedia.apps.android.countriesapp.data.ApolloCountryClient
+import com.appiwedia.apps.android.countriesapp.domain.CountryClient
+import com.appiwedia.apps.android.countriesapp.domain.GetCountriesUseCase
+import com.appiwedia.apps.android.countriesapp.domain.GetCountryUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl("https://countries.trevorblades.com/graphql")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountryClient(apolloClient: ApolloClient): CountryClient {
+        return ApolloCountryClient(apolloClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCountriesUseCase(countryClient: CountryClient): GetCountriesUseCase {
+        return GetCountriesUseCase(countryClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCountryUseCase(countryClient: CountryClient): GetCountryUseCase {
+        return GetCountryUseCase(countryClient)
+    }
+}
